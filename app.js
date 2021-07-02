81224b241ef501a147bbda4c9f09d98a4a97aa93
gabriel <gabeh.reiter@gmail.com>
2021-07-02 12:37:27 -0400

    root file added

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
@@ -8,7 +7,7 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
+
 app.get("/", (req, res) => res.send("Hello carlos"))
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
