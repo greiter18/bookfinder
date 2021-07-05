0a695b5ac9c91fc95e63cf0b0d36bd4ca50dda57
gabriel <gabeh.reiter@gmail.com>
2021-07-05 16:09:31 -0400

    back end routes runninmg

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI;
+const db = require('./config/key').mongoURI;
+const users = require("./routes/api/users");
+const books = require("./routes/api/books");
+
+app.use("/api/users", users);
+app.use("/api/books", books);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 //
-app.get("/", (req, res) => res.send("Hello carlos"))
+app.get("/", (req, res) => res.send("Hello Gabe"))
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
