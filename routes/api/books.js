b39c8687bd3703296e179bffbfac9c26b1d84d37
gabriel <gabeh.reiter@gmail.com>
2021-07-12 15:49:25 -0400

    tester added

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+// test route
 router.get("/test", (req, res) => res.json({ msg: "This is the books route" }));
 
 module.exports = router;
