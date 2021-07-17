229967570454f4281a5c2b95716102ed0a605e68
gabriel <gabeh.reiter@gmail.com>
2021-07-16 23:03:11 -0400

    book model added

---
+++ config/models/Books.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: {
+    type: string,
+    required: true
+  },
+  author: {
+    type: string,
+    required: true
+  },
+  image: {
+    type: string,
+    required: true
+  },
+  link: {
+    type: string,
+    required: true
+  }
+});
+
+const Book = mongose.model('book', BookSchema);
+
+module.exports = Book;
