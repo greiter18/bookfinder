f591a8e74aa7ead1951b6fa9f870f875430f7121
gabriel <gabeh.reiter@gmail.com>
2021-07-19 14:07:30 -0400

    added more files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 window.axios = axios;
---
+++ frontend/src/store/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/root_reducer';
+import logger from 'redux-logger'
+
+const configureStore = (preloadedState = {}) => {
+  let middleware = [thunk, logger];
+  if (process.env.NODE_ENV === 'production') {
+    middleware = [thunk];
+  }
+  return createStore(rootReducer, preloadedState, applyMiddleware(...middleware))
+};
+
+export default configureStore;
