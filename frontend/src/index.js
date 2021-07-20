80615bc0c0962af47a89c0c3a2fd0f2bece33ef8
Daniel Ahn <iamdanahn@gmail.com>
2021-07-20 14:20:04 -0400

    Todo notes in readme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import axios from "axios";
 import Root from "./root"
 // import configureStore from './store/store';
-
 // let store = configureStore({})
 
 ReactDOM.render(<Root/>, document.getElementById('root'));
