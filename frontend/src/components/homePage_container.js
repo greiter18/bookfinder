c94b1d7d0c80ef5eb01a0178c7379bc4cc351426
gabriel <gabeh.reiter@gmail.com>
2021-08-18 20:53:32 -0400

    changes to homepage re add book

--- frontend/src/components/homePage_container.js
+++ frontend/src/components/homePage_container.js
@@ -1,13 +1,14 @@
 import { connect } from "react-redux";
 import { withRouter } from 'react-router';
 import HomePage from "./homePage";
+import add_Book from "../actions/wishlist_action"
 
 const mstp = store => ({
   currentUser: store?.session?.user
 })
 
 const mdtp = dispatch => ({
-
+ addBook: bookId => dispatch(add_Book(bookId))
 })
 
 export default withRouter(connect(mstp,mdtp)(HomePage))
