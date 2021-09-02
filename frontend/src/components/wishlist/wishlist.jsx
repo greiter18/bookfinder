e86d6257a5e3ef96eca30e288a9afd5714fa7c30
gabriel <gabeh.reiter@gmail.com>
2021-09-01 20:29:22 -0400

    changes tp wishlistjsx

--- frontend/src/components/wishlist/wishlist.jsx
+++ frontend/src/components/wishlist/wishlist.jsx
@@ -2,14 +2,25 @@ import React, {useState, useEffect} from "react";
 import WishlistIndexItem from './wishlist_index_item';
 import axios from "axios";
 //hf5f3r38P1cC - tester
+//jWAsAAAAQBAJ
 
 const Wishlist = ({currentUser, wishlists, fetchWishlist, remove_book }) => {
-  const [books, setBooks] = useState([wishlists]);
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
     debugger
     fetchWishlist(currentUser.id)
   }, [])
+
+   useEffect(() => {
+    debugger
+    console.log('useeffect wishlist',wishlists)
+    let ids = wishlists.map(wishlist => {
+      //async await
+      return wishlist.book_id
+    })
+    setBooks(ids)
+  }, [wishlists])
 
   const options = {
     headers: {
@@ -17,16 +28,19 @@ const Wishlist = ({currentUser, wishlists, fetchWishlist, remove_book }) => {
     }
   };
 
-  const tester = () => {
-    return (
-      <h1>tester function </h1>
-    )
-  }
+  // const tester = () => {
+  //   return (
+  //     <h1>tester function </h1>
+  //   )
+  // }
 
   // const setbook = wishlists?.forEach(wishlist => {
-  // //put into
-  // setBooks([...books, wishlist]);
+  // // //put into
+  // setBooks([...books, wishlist.book_id]);
   // })
+  // 1 does the api take multiple requests?
+  //async await set up 
+  //Promise.all - takes multiple promise - only returns when all resolve
 
     // return (axios.get(`https://www.googleapis.com/books/v1/volumes/hf5f3r38P1cC`, options)
     //   .then((res) => {
@@ -46,7 +60,7 @@ const Wishlist = ({currentUser, wishlists, fetchWishlist, remove_book }) => {
       <h1>Wishlist page</h1>  
       {/* {wishlistList} */}
       {console.log('books',books)}
-      {tester()}
+      {/* {tester()} */}
       <WishlistIndexItem/>
     </div>
   )
