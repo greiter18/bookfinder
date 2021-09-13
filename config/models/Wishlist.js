e7b1231ccd32616cb0bea11768aac84efb5b3d7e
gabriel <gabeh.reiter@gmail.com>
2021-09-13 16:44:43 -0400

    changes to wishlist model

--- config/models/Wishlist.js
+++ config/models/Wishlist.js
@@ -10,6 +10,27 @@ const WishlistSchema = new Schema({
     type: String,
     ref: 'books' // confirm if this is correct???
   }
+  /*  user_id: {
+    type: String,
+     ref: 'users'
+   },
+    book_title: {
+     type: String,
+   }
+   book_author: {
+     type: String,
+   }
+  book_id: {
+    type: String,
+    ref: 'books' // confirm if this is correct???
+  },
+  book_image: {
+    type: String
+  }
+  book_link: {
+    type: String
+  }
+  */
 })
 
 const Wishlist = mongoose.model('wishlist', WishlistSchema);
